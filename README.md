ad8960ad83592cd058158559b3ffb5aea0c259e2
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2021-06-23 23:03:25 +0600

    Latest changes

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # Cow-weight-and-Breed-Prediction
-# Scraper-With-Puppeteer 
+
+## Scraper-With-Puppeteer 
 1. [A Guide to Web Scraping With JavaScript and Node.js](https://hackernoon.com/a-guide-to-web-scraping-with-javascript-and-nodejs-i21l3te1)
 2. [puppeteer](https://github.com/puppeteer/puppeteer)
 3. [puppeteer-cluster](https://github.com/thomasdondorf/puppeteer-cluster)
-4. [Slack Incoming Webhooks](https://www.npmjs.com/package/@slack/webhook)
-5. [Web Scraping with a Headless Browser: A Puppeteer Tutorial](https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial)
-
-```
-npm install axios
-npm install dotenv
-```
+4. [Web Scraping with a Headless Browser: A Puppeteer Tutorial](https://www.toptal.com/puppeteer/headless-browser-puppeteer-tutorial)
--- bm_cron.py
+++ bm_cron.py
@@ -1,21 +1,36 @@
+# import os
+# import time
+
+# from pathlib import Path
+
+# BASE_DIR = Path(__file__).resolve().parent
+
+# for offset in range(0,300):
+#     os.system(f'node cattle_link_scraper.js {offset}')
+#     file_name = BASE_DIR/"bm_cache"/f'card_{offset}.txt'
+#     if os.path.exists(file_name):
+#         with open(file_name, 'r') as f:
+#             content = f.read()
+#             if len(content) > 0:
+#                 print(f"Ok {offset}")
+#                 continue
+#             else:
+#                 os.system(f'node cattle_link_scraper.js {offset}')
+#     else:
+#         os.system(f'node cattle_link_scraper.js {offset}')
+#     time.sleep(20)
+
+
+
 import os
 import time
+import joblib
 
-from pathlib import Path
+url_set = joblib.load("pickles/url_set.pkl")
+print(len(url_set))
+time.sleep(20)
 
-BASE_DIR = Path(__file__).resolve().parent
-
-for offset in range(0,300):
-    os.system(f'node cattle_link_scraper.js {offset}')
-    file_name = BASE_DIR/"bm_cache"/f'card_{offset}.txt'
-    if os.path.exists(file_name):
-        with open(file_name, 'r') as f:
-            content = f.read()
-            if len(content) > 0:
-                print(f"Ok {offset}")
-                continue
-            else:
-                os.system(f'node cattle_link_scraper.js {offset}')
-    else:
-        os.system(f'node cattle_link_scraper.js {offset}')
+for offset, url in enumerate(url_set):
+    os.system(f'node scrape_details.js {url} {offset}')
+    print(f'Downloaded ===== {offset}')
     time.sleep(20)
