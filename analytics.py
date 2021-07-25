b6f7ae3887374495cbca45356587a183e29656ae
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2021-07-25 15:07:18 +0600

    Update on 514 cows

--- analytics.py
+++ analytics.py
@@ -1,6 +1,36 @@
 import os
 import glob
 
+
+def get_size(start_path="."):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    return total_size
+
+
+def bytes_to_unit(bytes, unit):
+    mapper = {
+        "B": "{:,.2f}".format(bytes) + " B",
+        "kb": "{:,.2f}".format(bytes / float(1 << 7)) + " kb",
+        "KB": "{:,.2f}".format(bytes / float(1 << 10)) + " KB",
+        "mb": "{:,.2f}".format(bytes / float(1 << 17)) + " mb",
+        "MB": "{:,.2f}".format(bytes / float(1 << 20)) + " MB",
+        "gb": "{:,.2f}".format(bytes / float(1 << 27)) + " gb",
+        "GB": "{:,.2f}".format(bytes / float(1 << 30)) + " GB",
+        "TB": "{:,.2f}".format(bytes / float(1 << 40)) + " TB",
+    }
+    key = str(unit.strip())
+    value = mapper.get(key, None)
+    if value:
+        return mapper[key]
+    else:
+        raise NotImplementedError(f"Provided unit `{unit}` is not implemented")
+
+
 total_yt_images = 0
 total_hd_images = 0
 for dirname, _, filenames in os.walk("yt_images"):
@@ -23,6 +53,10 @@ print(f"[INFO] total details collected => {total_detals}")
 print(f"[INFO] total hd images collected => {total_hd_images}")
 print(f"[INFO] total youtube images collected => {total_yt_images}")
 print(f"[INFO] total images collected => {total_images}")
+print("==========================================")
+print("=> Data directory is ", bytes_to_unit(get_size("./data"), "KB"))
+print("=> Image directory is ", bytes_to_unit(get_size("./images"), "MB"))
+print("=> Video directory is ", bytes_to_unit(get_size("./yt_videos"), "GB"))
 print("==========================================")
 print("Committing to github all the new changes >>>")
 print("==========================================")
