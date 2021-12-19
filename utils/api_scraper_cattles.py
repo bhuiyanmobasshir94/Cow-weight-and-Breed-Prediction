f805660cd25ab8add1ca206cc5608a5dcc7e8b88
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2021-12-19 14:44:27 +0600

    Update on api scrapper

--- utils/api_scraper_cattles.py
+++ utils/api_scraper_cattles.py
@@ -1,12 +1,19 @@
-import requests
-import joblib
-import os
 import json
+import os
 from collections import defaultdict
 from datetime import datetime
 
+import joblib
+import requests
+
+import analytics
+import api_scraper_details
+import scrape_youtube2images
+from configs import *
 
 DATA_DICT_LIST = []
+ext = str(datetime.today().date()).replace("-", "_")
+IMAGES_DICT = defaultdict(list)
 
 
 def scrape(url):
@@ -19,7 +26,7 @@ def download_image(img_url, img_name):
     img_bytes = requests.get(img_url).content
     with open(img_name, "wb") as img_file:
         img_file.write(img_bytes)
-        print(f"{img_name} was downloaded...")
+        logger.info(f"{img_name} was downloaded...")
 
 
 def all_cattles():
@@ -32,94 +39,102 @@ def all_cattles():
             json_response = scrape(url)
             DATA_DICT_LIST.extend(json_response["data"]["results"])
     assert (data_count - 1) == len(DATA_DICT_LIST)
-    print("========================================")
-    print("Data count => ", json_response["data"]["count"])
-    print("Downloaded data count => ", len(DATA_DICT_LIST))
-    print("========================================")
+    logger.info("========================================")
+    logger.info(f"Data count => %s",
+                json_response["data"]["count"], exc_info=1)
+    logger.info("Downloaded data count => %s", len(DATA_DICT_LIST), exc_info=1)
+    logger.info("========================================")
 
 
-all_cattles()
+def dump_data_dict():
+    joblib.dump(DATA_DICT_LIST,
+                f"{DATALAKE_DIR}/pickles/DATA_DICT_LIST_{ext}.pkl")
+    logger.info("Dumped data dict list")
 
 
-ext = str(datetime.today().date()).replace("-", "_")
+def dump_images_dict():
+    for value in DATA_DICT_LIST:
+        IMAGES_DICT[value["sku"]].append(value["thumbnail"])
+        for slide in value["slides"]:
+            IMAGES_DICT[value["sku"]].append(slide["image"])
+    joblib.dump(IMAGES_DICT, f"{DATALAKE_DIR}/pickles/IMAGES_DICT_{ext}.pkl")
+    logger.info("Dumped images dict")
 
 
-joblib.dump(DATA_DICT_LIST, f"pickles/DATA_DICT_LIST_{ext}.pkl")
-print("Dumped data dict list")
+def download_images():
+    IMAGES_DICT = dict(IMAGES_DICT)
+    for key, value in IMAGES_DICT.items():
+        if os.path.exists(f"{DATALAKE_DIR}/images/{key}"):
+            for index, img_url in enumerate(value):
+                img_name = f"{DATALAKE_DIR}/images/{key}/{key}_{index}.jpg"
+                if not os.path.exists(img_name):
+                    download_image(img_url, img_name)
+        else:
+            os.mkdir(f"{DATALAKE_DIR}/images/{key}")
+            for index, img_url in enumerate(value):
+                img_name = f"{DATALAKE_DIR}/images/{key}/{key}_{index}.jpg"
+                if not os.path.exists(img_name):
+                    download_image(img_url, img_name)
 
-IMAGES_DICT = defaultdict(list)
-for value in DATA_DICT_LIST:
-    IMAGES_DICT[value["sku"]].append(value["thumbnail"])
-    for slide in value["slides"]:
-        IMAGES_DICT[value["sku"]].append(slide["image"])
+
+def scrape_report():
+    image_count = 0
+    dir_count = 0
+    for dirname, _, filenames in os.walk(f"{DATALAKE_DIR}/images"):
+        # print(dirname)
+        # print(filenames)
+        if os.path.exists(dirname) and dirname is not "images":
+            dir_count += 1
+        for filename in filenames:
+            if os.path.exists(os.path.join(dirname, filename)):
+                image_count += 1
+
+    logger.info("========================================")
+    logger.info("Total images scraped => %s", image_count, exc_info=1)
+    logger.info("Total cow scraped => %s", dir_count, exc_info=1)
+    logger.info("========================================")
 
 
-joblib.dump(IMAGES_DICT, f"pickles/IMAGES_DICT_{ext}.pkl")
-print("Dumped images dict")
+def update_final_dict():
+    DATA_DICT_LIST = joblib.load(
+        f"{DATALAKE_DIR}/pickles/DATA_DICT_LIST_{ext}.pkl")
+    logger.info("Loaded data dict list")
+    IMAGES_DICT = joblib.load(f"{DATALAKE_DIR}/pickles/IMAGES_DICT_{ext}.pkl")
+    logger.info("Loaded images dict")
 
-
-IMAGES_DICT = dict(IMAGES_DICT)
-
-for key, value in IMAGES_DICT.items():
-    if os.path.exists(f"images/{key}"):
-        for index, img_url in enumerate(value):
-            img_name = f"images/{key}/{key}_{index}.jpg"
-            if not os.path.exists(img_name):
-                download_image(img_url, img_name)
+    if os.path.exists("{DATALAKE_DIR}/pickles/FINAL_DATA_DICT.pkl"):
+        FINAL_DATA_DICT = joblib.load(
+            "{DATALAKE_DIR}/pickles/FINAL_DATA_DICT.pkl")
+        logger.info("Loaded final data dict")
     else:
-        os.mkdir(f"images/{key}")
-        for index, img_url in enumerate(value):
-            img_name = f"images/{key}/{key}_{index}.jpg"
-            if not os.path.exists(img_name):
-                download_image(img_url, img_name)
+        FINAL_DATA_DICT = defaultdict(dict)
+        logger.info("Defined default final data dict")
+    for value in DATA_DICT_LIST:
+        value["images_list"] = IMAGES_DICT[value["sku"]]
+        FINAL_DATA_DICT[value["sku"]] = dict(value)
+
+    joblib.dump(FINAL_DATA_DICT, "{DATALAKE_DIR}/pickles/FINAL_DATA_DICT.pkl")
+    logger.info("Dumped final data dict")
+
+    logger.info("========================================")
+    logger.info("Final data dict total count => %s",
+                len(FINAL_DATA_DICT.keys()), exc_info=1)
+    logger.info("========================================")
 
 
-image_count = 0
-dir_count = 0
-for dirname, _, filenames in os.walk("images"):
-    # print(dirname)
-    # print(filenames)
-    if os.path.exists(dirname) and dirname is not "images":
-        dir_count += 1
-    for filename in filenames:
-        if os.path.exists(os.path.join(dirname, filename)):
-            image_count += 1
+# print("Shout out from detail scraper >>>")
 
-print("========================================")
-print("Total images scraped => ", image_count)
-print("Total cow scraped => ", dir_count)
-print("========================================")
+# print("Shout out from youtube videos to images scraper >>>")
+# print("========================================")
 
-DATA_DICT_LIST = joblib.load(f"pickles/DATA_DICT_LIST_{ext}.pkl")
-print("Loaded data dict list")
-IMAGES_DICT = joblib.load(f"pickles/IMAGES_DICT_{ext}.pkl")
-print("Loaded images dict")
+# print("Shout out from analytics >>>")
 
-if os.path.exists("pickles/FINAL_DATA_DICT.pkl"):
-    FINAL_DATA_DICT = joblib.load("pickles/FINAL_DATA_DICT.pkl")
-    print("Loaded final data dict")
-else:
-    FINAL_DATA_DICT = defaultdict(dict)
-    print("Defined default final data dict")
-for value in DATA_DICT_LIST:
-    value["images_list"] = IMAGES_DICT[value["sku"]]
-    FINAL_DATA_DICT[value["sku"]] = dict(value)
+# shutdown = input("\n Do you want me to end? Press y to proceed.\n >>> ")
 
-joblib.dump(FINAL_DATA_DICT, "pickles/FINAL_DATA_DICT.pkl")
-print("Dumped final data dict")
-
-print("========================================")
-print("Final data dict total count => ", len(FINAL_DATA_DICT.keys()))
-print("========================================")
-
-print("Shout out from detail scraper >>>")
-import api_scraper_details
-
-print("Shout out from youtube videos to images scraper >>>")
-print("========================================")
-import scrape_youtube2images
-
-print("Shout out from analytics >>>")
-import analytics
-
-shutdown = input("\n Do you want me to end? Press y to proceed.\n >>> ")
+if __name__ == "__main__":
+    all_cattles()
+    dump_data_dict()
+    dump_images_dict()
+    download_images()
+    scrape_report()
+    update_final_dict()
