62faa2c95cf507ff98e1e0d13f73eda48ffbe74e
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2021-12-23 23:57:18 +0600

    Updated files

---
+++ utils/dataloader.py
@@ -0,0 +1,39 @@
+import glob
+import os
+import sys
+
+import pandas as pd
+
+from configs import *
+from utilities import *
+
+in_file_name = os.path.join(DATALAKE_DIR, "dataset.csv")
+columns = ['sku', 'type', 'sex', 'color', 'breed', 'age_in_month',
+           'height_in_inch', 'weight_in_kg', 'price', 'size', 'images_count', 'yt_images_count']
+
+if not os.path.exists(in_file_name):
+    logger.warning("Dataset file not found")
+    logger.info("Downloading dataset, it may take 1 to 2 minutes")
+    download(DATASET_URL, in_file_name)
+
+if not len(glob.glob(f"{IMAGES_DIR}/*")) > 0:
+    logger.warning("Images not found")
+    logger.info("Downloading images, it may take 12 to 15 minutes")
+    download(IMAGES_URL, str(IMAGES_DIR))
+
+if not len(glob.glob(f"{YT_IMAGES_DIR}/*")) > 0:
+    logger.warning("YT images not found")
+    logger.info("Downloading yt_images, it may take 85 to 90 minutes")
+    download(YT_IMAGES_URL, str(YT_IMAGES_DIR))
+
+df = pd.read_csv(in_file_name, usecols=columns)
+logger.info(df.head())
+for index, row in df.iterrows():
+    images_path = os.path.join(IMAGES_DIR, row["sku"])
+    yt_images_path = os.path.join(YT_IMAGES_DIR, row["sku"])
+    all_images = []
+    if row['images_count'] > 0:
+        all_images.extend(glob.glob(f"{images_path}/*.jpg"))
+    if row['yt_images_count'] > 0:
+        all_images.extend(glob.glob(f"{yt_images_path}/*.jpg"))
+    logger.info(f"{row['sku']} has {len(all_images)} images")
--- utils/utilities.py
+++ utils/utilities.py
@@ -1,70 +1,26 @@
-import glob
 import os
 import sys
 
-import pandas as pd
+import requests
 
 from configs import *
+from extractor import extract
 
-file_name = os.path.join(DATALAKE_DIR, "pickles_to_df.csv")
-out_file_name = os.path.join(DATALAKE_DIR, "dataset.csv")
-columns = ['sku', 'type', 'sex', 'color', 'breed', 'age_in_month',
-           'height_in_inch', 'weight_in_kg', 'price', 'size']
 
-null_yt_images_count = 0
-null_yt_videos_count = 0
-null_images_count = 0
-null_pkl_count = 0
-exists_for_all = 0
+def download(url, file_path):
+    try:
+        gz_file_path = file_path + ".tar.gz"
+        r = requests.get(url, stream=True)
+        with open(gz_file_path, "wb") as f:
+            for chunk in r.raw.stream(1024, decode_content=False):
+                if chunk:
+                    f.write(chunk)
+        logger.info(
+            "Downloaded file from %s to %s", url, gz_file_path
+        )
+    except Exception as e:
+        logger.error(e)
+        sys.exit(1)
 
-if not os.path.exists(file_name):
-    logger.warning("Pickles to df file not found")
-    sys.exit(1)
-else:
-    df = pd.read_csv(file_name, usecols=columns)
-    df['images_count'] = 0
-    df['yt_images_count'] = 0
-    for index, row in df.iterrows():
-        images_path = os.path.join(IMAGES_DIR, row["sku"])
-        yt_images_path = os.path.join(YT_IMAGES_DIR, row["sku"])
-        yt_videos_path = os.path.join(YT_VIDEOS_DIR, row["sku"])
-        pickle_path = os.path.join(DETAILS_DIR, f"{row['sku']}.pkl")
-
-        if os.path.exists(images_path) or os.path.exists(yt_images_path) or os.path.exists(yt_videos_path) or os.path.exists(pickle_path):
-            if not os.path.exists(images_path) or (os.path.exists(images_path) and len(glob.glob(f"{images_path}/*.jpg")) == 0):
-                logger.info("Folder %s doesn't exist for images",
-                            row["sku"])
-                null_images_count += 1
-            else:
-                df.loc[index, 'images_count'] = len(
-                    glob.glob(f"{images_path}/*.jpg"))
-            if not os.path.exists(yt_images_path) or (os.path.exists(yt_images_path) and len(glob.glob(f"{yt_images_path}/*.jpg")) == 0):
-                logger.info("Folder %s doesn't exist for yt_images",
-                            row["sku"])
-                null_yt_images_count += 1
-            else:
-                df.loc[index, 'yt_images_count'] = len(
-                    glob.glob(f"{yt_images_path}/*.jpg"))
-            if not os.path.exists(yt_videos_path) or (os.path.exists(yt_videos_path) and len(glob.glob(f"{yt_videos_path}/*.mp4")) == 0):
-                logger.info("Folder %s doesn't exist for yt_videos",
-                            row["sku"])
-                null_yt_videos_count += 1
-            if not os.path.exists(pickle_path):
-                logger.info("Pickle file %s doesn't exist",
-                            row["sku"])
-                null_pkl_count += 1
-            if (os.path.exists(images_path) and os.path.exists(yt_images_path) and os.path.exists(yt_videos_path) and os.path.exists(pickle_path)) and (len(glob.glob(f"{images_path}/*.jpg")) > 0 and len(glob.glob(f"{yt_images_path}/*.jpg")) > 0 and len(glob.glob(f"{yt_videos_path}/*.mp4")) > 0):
-                exists_for_all += 1
-        else:
-            logger.info("Folder %s doesn't exist", row["sku"])
-    # logger.info(f"Columns: {df.columns.values}")
-    df.to_csv(out_file_name, index=False)
-    logger.info(df.head())
-    logger.info("Loaded the dataframe")
-    logger.info("Dataframe shape => %s", df.shape)
-
-logger.warning("Null yt images count => %s", null_yt_images_count)
-logger.warning("Null images count => %s", null_images_count)
-logger.warning("Null yt videos count => %s", null_yt_videos_count)
-logger.warning("Null pickles count => %s", null_pkl_count)
-logger.info("Exists for all count => %s", exists_for_all)
+    extract(DATALAKE_DIR, gz_file_path)
+    os.remove(gz_file_path)
