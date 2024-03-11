68a9eec71bed0835b8e5abba316240ba92c73f6b
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2024-03-11 12:06:12 +0600

    Updated stats

--- utils/dataset_generator.py
+++ utils/dataset_generator.py
@@ -7,10 +7,15 @@ dataset_dirs = ["data-2021", "data-2022", "data-2023"]
 HD_IMAGES_COUNT = 0
 YOUTUBE_VIDEOS_COUNT = 0
 TOTAL_DATA_COUNT = 0
+DF = None
 
 for dir in dataset_dirs:
     yearly_dir = BASE_DIR + "/" + dir
     df_csv = pd.read_csv(os.path.join(yearly_dir, "pickles_to_df.csv"))
+    if DF is not None:
+        DF = pd.concat([DF, df_csv], axis=0)
+    else:
+        DF = df_csv
     TOTAL_DATA_COUNT += len(df_csv)
     for index, row in df_csv.iterrows():
         sku = row["sku"]
@@ -23,3 +28,4 @@ for dir in dataset_dirs:
 print("Total Cow Data Count => ", TOTAL_DATA_COUNT)
 print("Total Cow HD Images => ", HD_IMAGES_COUNT)
 print("Total Cow Youtube Videos Count => ", YOUTUBE_VIDEOS_COUNT)
+print("Final Cow Data Count => ", len(DF))
