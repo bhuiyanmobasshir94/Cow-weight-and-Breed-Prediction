4a9e317681f9bc0c949a2d78e2c30c04c9110077
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2024-05-04 13:14:44 +0600

    New dataset updated

--- utils/dataset_generator.py
+++ utils/dataset_generator.py
@@ -60,6 +60,8 @@ for dir in dataset_dirs:
     rename_folders(f"{yearly_dir}/yt_videos/")
     df_csv = pd.read_csv(os.path.join(yearly_dir, "pickles_to_df.csv"))
     df_csv["sku"] = df_csv["sku"].str.replace(" ", "")
+    df_csv["images_count"] = 0
+    df_csv["yt_videos_count"] = 0
     if DF is not None:
         DF = pd.concat([DF, df_csv], axis=0)
     else:
@@ -68,60 +70,62 @@ for dir in dataset_dirs:
     for index, row in df_csv.iterrows():
         sku = row["sku"]
         images = glob.glob(f"{yearly_dir}/images/{sku}/*.jpg")
-        result = run_command(
-            command=[
-                "aws",
-                "s3",
-                "ls",
-                f"s3://{S3_BUCKET_NAME}/images/{sku}/",
-                "--human-readable",
-                "--summarize",
-            ]
-        )
-        pattern = r"Total Objects: (\d+)"
-        match = re.search(pattern, result.stdout)
-        if match:
-            count = match.group(1)
-            if len(images) != int(count):
-                print(f"Folder has {len(images)}, found {count} for {sku}")
-                run_command(
-                    command=[
-                        "aws",
-                        "s3",
-                        "cp",
-                        "--recursive",
-                        f"{yearly_dir}/images/{sku}",
-                        f"s3://{S3_BUCKET_NAME}/images/{sku}",
-                    ]
-                )
+        DF.loc[DF["sku"] == sku, "images_count"] = len(images)
+        # result = run_command(
+        #     command=[
+        #         "aws",
+        #         "s3",
+        #         "ls",
+        #         f"s3://{S3_BUCKET_NAME}/images/{sku}/",
+        #         "--human-readable",
+        #         "--summarize",
+        #     ]
+        # )
+        # pattern = r"Total Objects: (\d+)"
+        # match = re.search(pattern, result.stdout)
+        # if match:
+        #     count = match.group(1)
+        #     if len(images) != int(count):
+        #         print(f"Folder has {len(images)}, found {count} for {sku}")
+        #         run_command(
+        #             command=[
+        #                 "aws",
+        #                 "s3",
+        #                 "cp",
+        #                 "--recursive",
+        #                 f"{yearly_dir}/images/{sku}",
+        #                 f"s3://{S3_BUCKET_NAME}/images/{sku}",
+        #             ]
+        #         )
         HD_IMAGES_COUNT += len(images)
         videos = glob.glob(f"{yearly_dir}/yt_videos/{sku}/*.mp4")
-        result = run_command(
-            command=[
-                "aws",
-                "s3",
-                "ls",
-                f"s3://{S3_BUCKET_NAME}/videos/{sku}/",
-                "--human-readable",
-                "--summarize",
-            ]
-        )
-        pattern = r"Total Objects: (\d+)"
-        match = re.search(pattern, result.stdout)
-        if match:
-            count = match.group(1)
-            if len(videos) != int(count):
-                print(f"Folder has {len(videos)}, found {count} for {sku}")
-                run_command(
-                    command=[
-                        "aws",
-                        "s3",
-                        "cp",
-                        "--recursive",
-                        f"{yearly_dir}/yt_videos/{sku}",
-                        f"s3://{S3_BUCKET_NAME}/videos/{sku}",
-                    ]
-                )
+        DF.loc[DF["sku"] == sku, "yt_videos_count"] = len(videos)
+        # result = run_command(
+        #     command=[
+        #         "aws",
+        #         "s3",
+        #         "ls",
+        #         f"s3://{S3_BUCKET_NAME}/videos/{sku}/",
+        #         "--human-readable",
+        #         "--summarize",
+        #     ]
+        # )
+        # pattern = r"Total Objects: (\d+)"
+        # match = re.search(pattern, result.stdout)
+        # if match:
+        #     count = match.group(1)
+        #     if len(videos) != int(count):
+        #         print(f"Folder has {len(videos)}, found {count} for {sku}")
+        #         run_command(
+        #             command=[
+        #                 "aws",
+        #                 "s3",
+        #                 "cp",
+        #                 "--recursive",
+        #                 f"{yearly_dir}/yt_videos/{sku}",
+        #                 f"s3://{S3_BUCKET_NAME}/videos/{sku}",
+        #             ]
+        #         )
         YOUTUBE_VIDEOS_COUNT += len(videos)
 
 DF.to_csv(f"{BASE_DIR}/cow_dataset.csv", index=False)
