a0401df98df817834ef7c6db59436639748d65c2
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2024-05-10 10:00:17 +0600

    New datapoint added

--- run_scraper.sh
+++ run_scraper.sh
@@ -2,6 +2,6 @@ cd utils
 python api_scraper_cattles.py
 cd ../
 git add .
-git commit -m "New datapoint added"
+git commit -m 'New datapoint added'
 git push origin main
 python -m utils.dataset_generator
